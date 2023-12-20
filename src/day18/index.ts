453efb8813dfd04b28a501254ce88f3c408092e8
Jacob Dunn <jacob.dunn@aviture.us.com>
2023-12-19 22:19:55 -0600

    Day 18

--- src/day18/index.ts
+++ src/day18/index.ts
@@ -16,90 +16,174 @@ const transformByDirection: Record<
 
 const parseInput = (rawInput: string) =>
   rawInput.split("\n").map((row) => {
-    const [, direction, count, color] = row.match(
-      /([UDLR])\s(\d+)\s\((.+)\)/i
+    const [, direction, count] = row.match(
+      /([UDLR])\s(\d+)/
     ) as RegExpMatchArray;
     return {
       direction: direction as Direction,
       count: parseInt(count),
-      color,
     };
   });
 
-let testOutput = 0;
-const part1 = (rawInput: string) => {
-  // 7370: Low
-  // 18932: Low
-  const instructions = parseInput(rawInput);
-  const lagoon: Record<string, Record<string, boolean>> = {};
-  const steps = instructions.reduce<Direction[]>(
-    (combined, { count, direction }) => {
-      return [...combined, ...Array(count).fill(direction)];
-    },
-    []
-  );
-
-  const excavate = ({ x, y }: Coordinate) =>
-    (lagoon[y] = { ...lagoon[y], [x]: true });
-
-  let current: Coordinate = { x: 0, y: 0 };
-  let min: Coordinate = { x: 0, y: 0 };
-  let max: Coordinate = { x: 0, y: 0 };
-  excavate(current);
-  steps.forEach((step) => {
-    current = transformByDirection[step](current);
-    min = { x: Math.min(min.x, current.x), y: Math.min(min.y, current.y) };
-    max = { x: Math.max(max.x, current.x), y: Math.max(max.y, current.y) };
-    excavate(current);
+const digitToDirection: Record<string, Direction> = {
+  0: "R",
+  1: "D",
+  2: "L",
+  3: "U",
+};
+const parseRealInput = (rawInput: string) =>
+  rawInput.split("\n").map((row) => {
+    const [, count, direction] = row.match(
+      /\(\#(.{5})(.{1})\)/
+    ) as RegExpMatchArray;
+    return {
+      direction: digitToDirection[direction],
+      count: Number(`0x${count}`),
+    };
   });
 
-  const clearInterior = (coordinate: Coordinate) => {
-    if (lagoon[coordinate.y][coordinate.x]) {
-      return false;
+const area = (p: Coordinate, q: Coordinate, r: Coordinate) => {
+  return (q.y - p.y) * (r.x - q.x) - (q.x - p.x) * (r.y - q.y);
+};
+const isConvex = ([a, b, c]: Triangle) => {
+  return area(a, b, c) <= 0;
+};
+const pointInTriangle = ([a, b, c]: Triangle, point: Coordinate) => {
+  return (
+    (c.x - point.x) * (a.y - point.y) >= (a.x - point.x) * (c.y - point.y) &&
+    (a.x - point.x) * (b.y - point.y) >= (b.x - point.x) * (a.y - point.y) &&
+    (b.x - point.x) * (c.y - point.y) >= (c.x - point.x) * (b.y - point.y)
+  );
+};
+const getLeftmost = (coordinates: Coordinate[]) => {
+  return coordinates.reduce<[Coordinate, number]>(
+    ([leftmost, leftmostIndex], coordinate, index) =>
+      coordinate.x < leftmost.x ||
+      (coordinate.x === leftmost.x && coordinate.y < leftmost.y)
+        ? [coordinate, index]
+        : [leftmost, leftmostIndex],
+    [coordinates[0], 0]
+  );
+};
+
+const findEar = (polygon: Coordinate[]): [Triangle, number] => {
+  for (let midIndex = 0; midIndex < polygon.length; midIndex++) {
+    const prevIndex = midIndex <= 0 ? polygon.length - 1 : midIndex - 1;
+    const nextIndex = midIndex >= polygon.length - 1 ? 0 : midIndex + 1;
+
+    const prevVertex = polygon[prevIndex];
+    const midVertex = polygon[midIndex];
+    const nextVertex = polygon[nextIndex];
+
+    const indexes = [prevIndex, midIndex, nextIndex];
+    const triangle: Triangle = [prevVertex, midVertex, nextVertex];
+
+    if (!isConvex(triangle)) {
+      continue;
     }
-    excavate(coordinate);
-    return true;
-  };
-
-  let excavations = new Set<Coordinate>([
-    { x: parseInt(Object.keys(lagoon[min.y])[0]) + 1, y: min.y + 1 },
-  ]);
-  while (excavations.size > 0) {
-    [...excavations.values()].forEach((coordinate) => {
-      excavations.delete(coordinate);
-      const cleared = clearInterior(coordinate);
-      if (cleared) {
-        (["U", "D", "L", "R"] as Direction[]).forEach((direction) =>
-          excavations.add(transformByDirection[direction](coordinate))
-        );
-      }
-    });
+    if (
+      polygon.some(
+        (point, index) =>
+          !indexes.includes(index) && pointInTriangle(triangle, point)
+      )
+    ) {
+      continue;
+    }
+    return [triangle, midIndex];
   }
+  throw new Error("malformed");
+};
 
-  const output = [...Array(max.y - min.y + 1)]
-    .map((_, offsetY) => {
-      return [...Array(max.x - min.x + 1)]
-        .map((_, offsetX) => {
-          return lagoon[offsetY + min.y]?.[offsetX + min.x] ? "X" : ".";
-        })
-        .join("");
-    })
-    .join("\n");
+type Triangle = [Coordinate, Coordinate, Coordinate];
+const triangulate = (polygon: Coordinate[]) => {
+  const triangles: Triangle[] = [];
+  while (polygon.length > 3) {
+    const [triangle, midIndex] = findEar(polygon);
+    triangles.push(triangle);
+    polygon.splice(midIndex, 1);
+  }
+  triangles.push(polygon as Triangle);
+  return triangles;
+};
 
-  fs.writeFileSync(`./src/day18/test-${testOutput++}`, output);
+const areaOfTriangle = ([a, b, c]: Triangle) => {
+  return (a.x * (b.y - c.y) + b.x * (c.y - a.y) + c.x * (a.y - b.y)) * 0.5;
+};
 
-  const dug = Object.values(lagoon).reduce((sum, row) => {
-    return sum + Object.keys(row).length;
-  }, 0);
+const expandPolygon = (input: Coordinate[]) => {
+  const [, leftmostIndex] = getLeftmost(input);
+  const transforms: Coordinate[] = [
+    { x: 0, y: 0 },
+    { x: 1, y: 0 },
+    { x: 1, y: 1 },
+    { x: 0, y: 1 },
+  ];
 
-  return dug.toString();
+  const expanded = [...input];
+  let transformIndex = 0;
+  let index = leftmostIndex + 1;
+  let prevConvex = true;
+  do {
+    const prevVertex =
+      expanded[(index + expanded.length - 1) % expanded.length];
+    const midVertex = expanded[index % expanded.length];
+    const nextVertext = expanded[(index + 1) % expanded.length];
+
+    const nextConvex = isConvex([prevVertex, midVertex, nextVertext]);
+
+    if (prevConvex === nextConvex) {
+      transformIndex =
+        (transformIndex + transforms.length + (nextConvex ? 1 : -1)) %
+        transforms.length;
+    }
+    expanded[index % expanded.length] = {
+      x: midVertex.x + transforms[transformIndex].x,
+      y: midVertex.y + transforms[transformIndex].y,
+    };
+
+    prevConvex = nextConvex;
+    index = (index + 1) % expanded.length;
+  } while (index !== leftmostIndex);
+
+  return expanded;
+};
+
+const part1 = (rawInput: string) => {
+  const instructions = parseInput(rawInput);
+  return digLagoon(instructions);
 };
 
 const part2 = (rawInput: string) => {
-  const input = parseInput(rawInput);
-
-  return;
+  const instructions = parseRealInput(rawInput);
+  return digLagoon(instructions);
 };
+
+const digLagoon = (instructions: ReturnType<typeof parseInput>) => {
+  const polygon = instructions.reduce<Coordinate[]>(
+    (points, instruction, index) => {
+      if (index === instructions.length - 1) {
+        return points;
+      }
+      const previous = points[points.length - 1];
+      const next = transformByDirection[instruction.direction](
+        previous,
+        instruction.count
+      );
+
+      points.push(next);
+      return points;
+    },
+    [{ x: 0, y: 0 }]
+  );
+  const expanded = expandPolygon(polygon);
+  const triangles = triangulate(expanded);
+  const area = triangles.reduce(
+    (sum, triangle) => sum + areaOfTriangle(triangle),
+    0
+  );
+  return area.toString();
+};
+
 const input = `
   R 6 (#70c710)
   D 5 (#0dc571)
@@ -122,15 +206,23 @@ run({
         input,
         expected: "62",
       },
+      {
+        input: `
+        R 6 (#70c710)
+        D 5 (#0dc571)
+        L 6 (#5713f0)
+        U 5 (#d2c081)`,
+        expected: "42",
+      },
     ],
     solution: part1,
   },
   part2: {
     tests: [
-      // {
-      //   input: ``,
-      //   expected: "",
-      // },
+      {
+        input,
+        expected: "952408144115",
+      },
     ],
     solution: part2,
   },
